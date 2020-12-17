2daffba55ae8db9ef809ce3a8c055b770b88a5c3
ChrisMethsillo <christian.parra@ug.uchile.cl>
2020-12-17 10:39:03 -0300

    Se modifica la clase CamWindow, de forma que esta reciba como parametro una ventana externa (Con la estructura de la Main Window encontrada en DuckieGUI.py) para ejecutar distintas interacciones entre ellas, la principal, poder modificar labels de la ventana recibida como parametro

--- lib/camera_chooser.py
+++ lib/camera_chooser.py
@@ -5,12 +5,13 @@ import subprocess
 
 class CamWindow(QWidget):
     #Main Window
-    def __init__(self, *args, **kwargs):
+    def __init__(self,mainwindow, *args, **kwargs):
         super(CamWindow, self).__init__(*args, **kwargs)
         #Estructura de la ventana principal
         self.x=430
         self.y=250
         self.cameralabel='Duckiebot'
+        self.window=mainwindow
 
         self.camera='human'
         self.distortion="0"
@@ -59,6 +60,7 @@ class CamWindow(QWidget):
         self.collision="0"
         self.cameralabel='top view'
         self.label.setText("Chosen view: "+self.cameralabel)
+        self.window.label2.setText("Camera: "+self.cameralabel)
 
     def human_camera(self):
         self.camera="human"
@@ -66,6 +68,7 @@ class CamWindow(QWidget):
         self.collision="0"
         self.cameralabel='Duckiebot'
         self.label.setText("Chosen view: "+self.cameralabel)
+        self.window.label2.setText("Camera: "+self.cameralabel)
 
     def fish_camera(self):
         self.camera="human"
@@ -73,6 +76,7 @@ class CamWindow(QWidget):
         self.collision="0"
         self.cameralabel='Fish Eye'
         self.label.setText("Chosen view: "+self.cameralabel)
+        self.window.label2.setText("Camera: "+self.cameralabel)
 
     def collision_box(self):
         self.camera="human"
@@ -80,6 +84,7 @@ class CamWindow(QWidget):
         self.collision="1"
         self.cameralabel='Collision box'
         self.label.setText("Chosen view: "+self.cameralabel)
+        self.window.label2.setText("Camera: "+self.cameralabel)
 
     def roadline_set(self):
         if self.roadline=="0":
